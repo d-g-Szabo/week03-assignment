632cc3536f76378d1074f900f0c513ef307785d0
Daniel G Szabo <d.g.szabo-2017@hull.ac.uk>
2024-06-08 14:42:59 +0100

    refactor the code to make it more readable

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-// global variable to store the value of the cookies
-
-// DOM manipulation:
-// cookie counter
-// cps (cookies per second)
-// image to click on
-// select these elements from the DOM and store them in variables
-let cookieCountertext = document.querySelector(".cookie-ammount");
-let cpsText = document.querySelector(".cookie-per-second");
-let cookieImg = document.getElementById("cookie-image");
-const resetButton = document.getElementById("reset");
-const cheatButton = document.getElementById("cheat");
-
-resetButton.addEventListener("click", function () {
-  resetGame();
-});
-
-cheatButton.addEventListener("click", function () {
-  gamedata.cookieCounter = 5000000;
-  updateDisplay();
-});
-
-// a way to store the shop items that we get from the API
-let shopItems = [];
+// have all the game information in one object
+// game information: cookie counter, cps, shop items
+let gamedata = {
+  cookieCounter: 0,
+  cps: 0,
+  shopItems: [],
+};
 
 // fetch the shop items from the API
-// a way to store the shop items that we get from the API (async, await)
 async function getShopItems() {
   // fetch the shop items from the API
   const response = await fetch(
@@ -38,42 +20,21 @@ async function getShopItems() {
   // ! for some reason the data is pushed into the array on the 0 index rather then populate the array, still can use it but need to use two indexes to get the data (shopItems[0][0])
   shopItems.push(data);
 
+  // call the game function
   game();
 }
 
-getShopItems();
-
-// an event listener for clicking on the cookie
-cookieImg.addEventListener("click", function () {
-  // increment the cookie counter by 1
-  gamedata.cookieCounter++;
-  // update the cookie counter text
-  updateDisplay();
-});
-
-// have all the game information in one function(object?)
-// game information: cookie counter, cps, shop items
-let gamedata = {
-  cookieCounter: 0,
-  cps: 0,
-  shopItems: [],
-};
-
 // game function
 function game() {
-  // you need to check if there are any cookies in the local storage(cookieCounter, cps)
-
   // if there are cookies in the local storage, load the game information
   if (localStorage.getItem("savedGame")) {
     // load the game information
-    // load the shop items
     loadGame();
   }
   // if there are no cookies in the local storage, start a new game
   else {
     resetGame();
   }
-
   // render the shop items
   renderShop();
   // update the display
@@ -81,16 +42,14 @@ function game() {
 }
 
 // load the game information from the local storage
-// load the game --> loadGame() calls the game function
-// fetch the shop items from the API
-// render(display) the shop items on the page
 function loadGame() {
   // load the game information from the local storage
   const saveData = localStorage.getItem("savedGame");
+  // parse the data from a string to the gamedata object
   gamedata = JSON.parse(saveData);
-  // load the shop items
 }
 
+// reset the game
 function resetGame() {
   gamedata.cookieCounter = 0;
   gamedata.cps = 0;
@@ -98,18 +57,15 @@ function resetGame() {
   updateDisplay();
 }
 
-// we need a timer to increase the cookies we get every second (cps)
-setInterval(function () {
-  // increase the cookie counter by one every second
-  gamedata.cookieCounter += gamedata.cps;
-  // update the value displayed on the page (or could have this in a separate function to call inside the interv, for example updateCookieCounter())
-  updateDisplay();
-  // update the value in the local storage (or could have this in a separate function to call inside the interv, for example saveGame())
-  saveGames();
-}, 1000);
+// save the game information in the local storage
+function saveGames() {
+  // turn the gamedata object into a string
+  const saveData = JSON.stringify(gamedata);
+  // save the string in the local storage
+  localStorage.setItem("savedGame", saveData);
+}
 
-// extra tools, if you want to use them to separate different tasks into functions
-
+// update the display
 function updateDisplay() {
   // update the DOM element containing the value of cookie counter
   cookieCountertext.textContent = gamedata.cookieCounter;
@@ -124,15 +80,6 @@ function updateDisplay() {
       : 0;
   }
 }
-
-function saveGames() {
-  // a method to turn your data into strings
-  const saveData = JSON.stringify(gamedata);
-  localStorage.setItem("savedGame", saveData);
-  // a method to set the items using key and value in the local storage
-}
-
-const shopTable = document.querySelector("table");
 
 function renderShop() {
   for (let index = 0; index < shopItems[0].length; index++) {
@@ -166,6 +113,50 @@ function renderShop() {
     shopTable.append(shopItem);
   }
 }
+
+// select these elements from the DOM and store them in variables
+let cookieCountertext = document.querySelector(".cookie-ammount");
+let cpsText = document.querySelector(".cookie-per-second");
+let cookieImg = document.getElementById("cookie-image");
+const resetButton = document.getElementById("reset");
+const cheatButton = document.getElementById("cheat");
+
+resetButton.addEventListener("click", function () {
+  resetGame();
+});
+
+cheatButton.addEventListener("click", function () {
+  gamedata.cookieCounter = 5000000;
+  updateDisplay();
+});
+
+// a way to store the shop items that we get from the API
+let shopItems = [];
+
+// call the function to fetch the shop items
+getShopItems();
+
+// create a table to display the shop items
+// has to be called after the shopItems array is populated
+const shopTable = document.querySelector("table");
+
+// an event listener for clicking on the cookie
+cookieImg.addEventListener("click", function () {
+  // increment the cookie counter by 1
+  gamedata.cookieCounter++;
+  // update the cookie counter text
+  updateDisplay();
+});
+
+// we need a timer to increase the cookies we get every second (cps)
+setInterval(function () {
+  // increase the cookie counter by one every second
+  gamedata.cookieCounter += gamedata.cps;
+  // update the value displayed on the page (or could have this in a separate function to call inside the interv, for example updateCookieCounter())
+  updateDisplay();
+  // update the value in the local storage (or could have this in a separate function to call inside the interv, for example saveGame())
+  saveGames();
+}, 1000);
 
 // event listener for buying the shop items
 // shop items should increase the cps
